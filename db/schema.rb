2f881969472f9bd96cd3774d88934b1fac821474
katonaoya <kato7080220@yahoo.co.jp>
2021-10-06 22:59:55 +0900

    profile_finish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_01_073522) do
+ActiveRecord::Schema.define(version: 2021_10_06_075302) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2021_10_01_073522) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.string "icon"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
